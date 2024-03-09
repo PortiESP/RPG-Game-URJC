c33e2fdb9252c925f8b339ddaa65297466e5b167
CuB1z <cub1zdev@gmail.com>
2024-03-09 20:18:28 +0100

    Refactor FileManager class by removing unused methods

--- utils/FileManager.java
+++ utils/FileManager.java
@@ -2,12 +2,8 @@ package utils;
 
 /*
     Auxiliary class containing the methods to save and read files
-
     - saveFile: Method to save a game in a file
     - readFile: Method to read a game from a file
-    - getActualDate: Method to get the actual date in the format YYYY-MM-DD_hh-mm-ss
-
-    Actual date is used as a file name to avoid overwriting files errors
 */
 
 import src.Game;

bf611d593f1cd83536948af4539a249dc6333987
CuB1z <cub1zdev@gmail.com>
2024-03-09 19:53:28 +0100

    Added attributes and getters & setters to users...

--- src/users/Admin.java
+++ src/users/Admin.java
@@ -2,4 +2,8 @@ package src.users;
 
 public class Admin extends User {
     
+    // Constructor ========================================================================================================
+    public Admin(String name, String nick, String password) {
+        super(name, nick, password);
+    }
 }
--- utils/Constants.java
+++ utils/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     
     // Constants for the file paths
     public static final String DATA_PATH = "/data/";
+
+    // Constants for the game rules
+    public static final int INITIAL_GOLD = 500;
 }

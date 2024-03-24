ba48e6a64e8774530e7bf3e6564b2bec848d6f81
danisntoss <danisant1504.dsl@gmail.com>
2024-03-24 19:19:34 +0100

    added function loadFromArray to talent

--- src/abilities/Talent.java
+++ src/abilities/Talent.java
@@ -1,10 +1,25 @@
 package src.abilities;
 
+import java.util.ArrayList;
+
 public class Talent extends SpecialAbility {
 
     // ============================================================================================[ Constructor ]>>>
     public Talent() {
         super();
     }
-    
+    public Talent(String name, int attack, int defense) {
+        super(name, attack, defense);
+    }
+
+    // ============================================================================================[ Static Methods ]>>>
+    public static ArrayList<Talent> loadFromArray(String[][] talentsArr) {
+        ArrayList<Talent> talents = new ArrayList<>();
+        for (String[] talent : talentsArr) {
+            talents.add(new Talent(talent[0], Integer.parseInt(talent[1]), Integer.parseInt(talent[2])));
+        }
+
+        return talents;
+    }
+
 }

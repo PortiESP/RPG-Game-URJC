1eb6ede07ffda4198493bb617c0c63f89d0ff08d
LuisAbril <luisabrilandreu@gmail.com>
2024-04-22 18:05:26 +0200

    testWeapons

--- tests/Test_Weapon.java
+++ tests/Test_Weapon.java
@@ -2,23 +2,36 @@ package tests;
 
 import org.junit.Test;
 
+import src.equipment.Weapon;
+
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 class Test_Weapon {
 
     @Test
-    void testToString() {
+    public void testToString() {
+        return super.toString() + " (Hands Required: " + this.handsRequired + ")";
     }
 
     @Test
-    void loadFromArray() {
+    public static ArrayList<Weapon> loadFromArray(String[][] arr) {
+        ArrayList<Weapon> weapons = new ArrayList<>();
+        for (String[] weapon : arr) {
+            weapons.add(new Weapon(weapon[0], Integer.parseInt(weapon[1]), Integer.parseInt(weapon[2]), Integer.parseInt(weapon[3])));
+        }
+
+        return weapons;
     }
 
     @Test
-    void getHandsRequired() {
+    public int getHandsRequired() {
+        return handsRequired;
     }
 
     @Test
-    void setHandsRequired() {
+    public void setHandsRequired(int handsRequired) {
+        this.handsRequired = handsRequired;
     }
 }

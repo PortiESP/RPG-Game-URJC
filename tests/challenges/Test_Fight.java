114586d0a8a3e1e5ab6036b68214497f1287658d
LuisAbril <luisabrilandreu@gmail.com>
2024-04-26 13:42:55 +0200

    4 tests no funcionan correctamente porque se llama a clase abstracta

--- tests/challenges/Test_Fight.java
+++ tests/challenges/Test_Fight.java
@@ -1,63 +1,131 @@
 package tests.challenges;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 
-class Test_Fight {
+import src.challenges.Fight;
+import src.users.Player;
+import src.characters.Character;
+import utils.MenuBuilder;
 
-    @Test
-    @Disabled
-    void getRounds() {}
 
-    @Test
-    @Disabled
-    void setRounds() {}
+public class Test_Fight {
 
     @Test
-    @Disabled
-    void getDate() {}
+    public void testGetRounds() {
+        Fight fight = new Fight();
+        fight.setRounds(5);
+        Assertions.assertEquals(5, fight.getRounds());
+    }
 
     @Test
-    @Disabled
-    void setDate() {}
+    public void testGetDate() {
+        Fight fight = new Fight();
+        fight.setDate("2022-01-01");
+        Assertions.assertEquals("2022-01-01", fight.getDate());
+    }
 
     @Test
-    @Disabled
-    void getWinner() {}
+    public void testGetWinner() {
+        Fight fight = new Fight();
+        Player winner = new Player();
+        fight.setWinner(winner);
+        Assertions.assertEquals(winner, fight.getWinner());
+    }
 
     @Test
-    @Disabled
-    void setWinner() {}
+    public void testGetLog() {
+        Fight fight = new Fight();
+        ArrayList<String> log = new ArrayList<>();
+        log.add("Round 1");
+        log.add("Round 2");
+        fight.setLog(log);
+        Assertions.assertEquals(log, fight.getLog());
+    }
 
     @Test
-    @Disabled
-    void getLog() {}
+    public void testGetC1() {
+        Fight fight = new Fight();
+        Character c1 = new Character();
+        fight.setC1(c1);
+        Assertions.assertEquals(c1, fight.getC1());
+    }
 
     @Test
-    @Disabled
-    void setLog() {}
+    public void testGetC2() {
+        Fight fight = new Fight();
+        Character c2 = new Character();
+        fight.setC2(c2);
+        Assertions.assertEquals(c2, fight.getC2());
+    }
 
     @Test
-    @Disabled
-    void getC1() {}
+    public void testGetPlayers() {
+        Fight fight = new Fight();
+        Player[] players = new Player[2];
+        players[0] = new Player();
+        players[1] = new Player();
+        fight.setPlayers(players);
+        Assertions.assertArrayEquals(players, fight.getPlayers());
+    }
 
     @Test
-    @Disabled
-    void setC1() {}
+    public void testSetRounds() {
+        Fight fight = new Fight();
+        fight.setRounds(10);
+        Assertions.assertEquals(10, fight.getRounds());
+    }
 
     @Test
-    @Disabled
-    void getC2() {}
+    public void testSetDate() {
+        Fight fight = new Fight();
+        fight.setDate("2022-02-01");
+        Assertions.assertEquals("2022-02-01", fight.getDate());
+    }
 
     @Test
-    @Disabled
-    void setC2() {}
+    public void testSetWinner() {
+        Fight fight = new Fight();
+        Player winner = new Player();
+        fight.setWinner(winner);
+        Assertions.assertEquals(winner, fight.getWinner());
+    }
 
     @Test
-    @Disabled
-    void getPlayers() {}
+    public void testSetLog() {
+        Fight fight = new Fight();
+        ArrayList<String> log = new ArrayList<>();
+        log.add("Round 1");
+        log.add("Round 2");
+        fight.setLog(log);
+        Assertions.assertEquals(log, fight.getLog());
+    }
 
     @Test
-    @Disabled
-    void setPlayers() {}
+    public void testSetC1() {
+        Fight fight = new Fight();
+        Character c1 = new Character();
+        fight.setC1(c1);
+        Assertions.assertEquals(c1, fight.getC1());
+    }
+
+    @Test
+    public void testSetC2() {
+        Fight fight = new Fight();
+        Character c2 = new Character();
+        fight.setC2(c2);
+        Assertions.assertEquals(c2, fight.getC2());
+    }
+
+    @Test
+    public void testSetPlayers() {
+        Fight fight = new Fight();
+        Player[] players = new Player[2];
+        players[0] = new Player();
+        players[1] = new Player();
+        fight.setPlayers(players);
+        Assertions.assertArrayEquals(players, fight.getPlayers());
+    }
 }

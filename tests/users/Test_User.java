7af02f32a770a306d7c76ef78f53e6a34dab6414
Porti <admin@0xPorti.com>
2024-04-29 17:26:50 +0200

    User getters and setters tests

--- tests/users/Test_User.java
+++ tests/users/Test_User.java
@@ -1,35 +1,96 @@
+
 package tests.users;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
+import src.users.Player;
+import utils.Const;
 
-class Test_User {
-
-    @Test
-    @Disabled
-    void getScore() {}
+public class Test_User {
 
     @Test
-    @Disabled
-    void getName() {}
+    public void getScoreTest() {
+        // Create an instance of User
+        Player user = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the getScore method
+        int score = user.getScore();
+
+        // Assert that the score is the initial gold
+        assertEquals(Const.INITIAL_GOLD, score);
+    }
 
     @Test
-    @Disabled
-    void setName() {}
+    public void getName() {
+        String name = "Name";
+        
+        // Create an instance of User
+        Player player = new Player(name, "Nick", "Password", "A11A1");
+
+        // Assert that the name is null
+        assertEquals(name, player.getName());
+    }
 
     @Test
-    @Disabled
-    void getNick() {}
+    public void setName() {
+        // Create an instance of User
+        Player user = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the setName method with a name
+        user.setName("John");
+
+        // Assert that the name is "John"
+        assertEquals("John", user.getName());
+    }
 
     @Test
-    @Disabled
-    void setNick() {}
+    public void getNick() {
+        // Create an instance of User
+        Player player = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the getNick method
+        String nick = player.getNick();
+
+        // Assert that the nick "Nick"
+        assertEquals("Nick", nick);
+    }
 
     @Test
-    @Disabled
-    void getPassword() {}
+    public void setNick() {
+        // Create an instance of User
+        Player player = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the setNick method with a nick
+        player.setNick("nick");
+
+        // Assert that the nick is "nick"
+        assertEquals("nick", player.getNick());
+    }
 
     @Test
-    @Disabled
-    void setPassword() {}
+    public void getPassword() {
+        // Create an instance of User
+        Player player = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the getPassword method
+        String password = player.getPassword();
+
+        // Assert that the password is "Password"
+        assertEquals("Password", password);
+    
+    }
+
+    @Test
+    public void setPassword() {
+        // Create an instance of User
+        Player player = new Player("Name", "Nick", "Password", "A11A1");
+
+        // Call the setPassword method with a password
+        player.setPassword("password");
+
+        // Assert that the password is "password"
+        assertEquals("password", player.getPassword());
+    }
+
 }
